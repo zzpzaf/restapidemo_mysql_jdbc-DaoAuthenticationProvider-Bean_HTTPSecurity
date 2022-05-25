145a4391f45346c3378873fdff93e15842fc5389
Panos Zafeiropoulos <zzpzaf@gmail.com>
2022-05-25 20:23:05 +0300

    session stateless

--- src/main/java/com/zzpzaf/restapidemo/Configuration/CustomSecurityConfiguration.java
+++ src/main/java/com/zzpzaf/restapidemo/Configuration/CustomSecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,6 +23,7 @@ public class CustomSecurityConfiguration extends WebSecurityConfigurerAdapter {
        @Override
        protected void configure(HttpSecurity http) throws Exception {
                http.authorizeRequests().antMatchers("/api/items").hasRole("ADMIN")
+               .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                .and().httpBasic();
        }
 
