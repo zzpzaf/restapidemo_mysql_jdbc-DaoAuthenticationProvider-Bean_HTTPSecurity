9ee35c1c8464e48ee5954644452ba8653b82009f
Panos Zafeiropoulos <zzpzaf@gmail.com>
2022-05-25 20:16:57 +0300

    JdbcDaoImpl bean

--- src/main/java/com/zzpzaf/restapidemo/Configuration/CustomSecurityConfiguration.java
+++ src/main/java/com/zzpzaf/restapidemo/Configuration/CustomSecurityConfiguration.java
@@ -1,10 +1,16 @@
 package com.zzpzaf.restapidemo.Configuration;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 
@@ -15,13 +21,27 @@ public class CustomSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
        @Override
        protected void configure(HttpSecurity http) throws Exception {
-               http.authorizeRequests().antMatchers("/api/items").hasRole("USER").and().httpBasic();
+               http.authorizeRequests().antMatchers("/api/items").hasRole("ADMIN")
+               .and().httpBasic();
        }
 
-       @Override
-       protected void configure(AuthenticationManagerBuilder auth) throws Exception {                
-               auth.inMemoryAuthentication()
-               .withUser("user").password("{noop}mypassword").roles("USER");
-       }
+       // @Override
+       // protected void configure(AuthenticationManagerBuilder auth) throws Exception {                
+       //         auth.inMemoryAuthentication()
+       //         .withUser("user").password("{noop}mypassword").roles("USER");
+       // }
+
+        @Bean
+        public JdbcDaoImpl userDetailsService(DataSource dataSource) {
+                JdbcDaoImpl jdbcDaoImpl = new JdbcDaoImpl();
+                jdbcDaoImpl.setDataSource(dataSource);
+                return jdbcDaoImpl;
+        } 
+        
+        @Bean
+        public PasswordEncoder encoder() {
+            return new BCryptPasswordEncoder();
+        }
+
 }
 
