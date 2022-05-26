2c3717cf1ce9e524618d30a4b4d6318396704b54
Panos Zafeiropoulos <zzpzaf@gmail.com>
2022-05-26 10:09:52 +0300

    AuthenticationManagerBuilder jdbcAuthentication() configurer with JdbcTemplate

--- src/main/java/com/zzpzaf/restapidemo/Configuration/CustomSecurityConfiguration.java
+++ src/main/java/com/zzpzaf/restapidemo/Configuration/CustomSecurityConfiguration.java
@@ -1,36 +1,45 @@
 package com.zzpzaf.restapidemo.Configuration;
 
-import javax.sql.DataSource;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-
 
 
 @Configuration
 @EnableWebSecurity
 public class CustomSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;    
 
+    //Authentication and Authorization SQL Strings
+    private final String authenticateSQL = "SELECT USERNAME as user_name, PASSWORD as user_pwd, ACCOUNT_ENABLED as user_enabled FROM USERS WHERE USERNAME = ?";
+    private final String authorizeSQL = "SELECT USERNAME as user_name, ROLE as user_role FROM USERSROLES WHERE USERNAME = ?";
+ 
        @Override
        protected void configure(HttpSecurity http) throws Exception {
-               http.authorizeRequests().antMatchers("/api/items").hasRole("ADMIN")
-               .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-               .and().httpBasic();
+            http.authorizeRequests().antMatchers("/api/items").hasRole("ADMIN")
+            .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and().httpBasic();
        }
 
-        @Bean
-        public UserDetailsService userDetailsService(DataSource dataSource) {
-            return new JdbcUserDetailsManager(dataSource);
-        }
+       @Override
+       public void configure(AuthenticationManagerBuilder auth) throws Exception {
+           auth.jdbcAuthentication()
+           .dataSource(jdbcTemplate.getDataSource())     //database connection
+           .usersByUsernameQuery(authenticateSQL)
+           .authoritiesByUsernameQuery(authorizeSQL)
+           .passwordEncoder(encoder());
+       }
+
         
         @Bean
         public PasswordEncoder encoder() {
